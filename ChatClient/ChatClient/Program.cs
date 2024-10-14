3e3451ead3601d3a8569c44ae70c0ceead4f4ac7
ferencprem2 <fercsusz0508@gmail.com>
2024-10-14 12:36:31 +0200

    asd

---
+++ ChatClient/ChatClient/Program.cs
@@ -0,0 +1,75 @@
+﻿using System.Net.Sockets;
+using System.Text;
+
+namespace ChatClient;
+
+class Program
+{
+    static TcpClient _tcpClient;
+    static Stream _tcpStream;
+    static void Main(string[] args)
+    {
+
+        Console.WriteLine("Enter server ip address: ");
+        string serverIp = Console.ReadLine();
+        
+        _tcpClient = new TcpClient();
+        _tcpClient.Connect(serverIp, 1010);
+        Console.WriteLine("Connected to server");
+        
+        _tcpStream = _tcpClient.GetStream();
+
+        Thread recieveThread = new Thread(RecieveMessage);
+        Thread sendThread = new Thread(SendMessage);
+        
+        recieveThread.Start();
+        sendThread.Start();
+    }
+
+    static void RecieveMessage()
+    {
+        byte[] buffer = new byte[1024];
+        while (true)
+        {
+            try
+            {
+                int bytesRead = _tcpStream.Read(buffer, 0, buffer.Length);
+                if (bytesRead > 0)
+                {
+                    string message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                    Console.WriteLine($"Server: {message}");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Connection closed by server");
+                throw;
+            }
+        }
+    }
+
+    static void SendMessage()
+    {
+        Console.Write("Név: ");
+        string name = Console.ReadLine();
+    
+        while (true)
+        {
+            Console.Write($"{name}: ");
+            
+            int cursorTopBeforeInput = Console.CursorTop;
+            
+            string message = Console.ReadLine();
+            
+            Console.SetCursorPosition(0, cursorTopBeforeInput); 
+            Console.Write(new string(' ', Console.WindowWidth));  
+            Console.SetCursorPosition(0, cursorTopBeforeInput);  
+            
+            Console.WriteLine($"{name}: {message}");
+            
+            byte[] buffer = Encoding.UTF8.GetBytes($"{name}: {message}");
+            _tcpStream.Write(buffer, 0, buffer.Length);
+        }
+    }
+
+}
---
+++ CmdGroupchat/ConsoleApp1/Program.cs
@@ -0,0 +1,63 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ConsoleApp1;
+
+class Program
+{
+    static TcpClient tcpClient;
+    static TcpListener tcpListener;
+    
+    static void Main(string[] args)
+    {
+        tcpListener = new TcpListener(IPAddress.Any, 1010);
+        tcpListener.Start();
+        Console.WriteLine("Server started on port: 42069");
+        
+        tcpClient = tcpListener.AcceptTcpClient();
+        Console.WriteLine("Client Connected");
+
+        Thread recieveThread = new Thread(RecieveMessage);
+        Thread sendThread = new Thread(SendMessage);
+        
+        recieveThread.Start();
+        sendThread.Start();
+    }
+
+    static void RecieveMessage()
+    {
+        NetworkStream stream = tcpClient.GetStream();
+        byte[] buffer = new byte[1024];
+        while (true)
+        {
+            try
+            {
+                int bytesRead = stream.Read(buffer, 0, buffer.Length);
+                if (bytesRead > 0)
+                {
+                    string message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                    Console.WriteLine($"{message}");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Client Disconnected");
+                throw;
+            }
+        }
+    }
+
+    static void SendMessage()
+    {
+        NetworkStream stream = tcpClient.GetStream();
+        while (true)
+        {
+            string message = Console.ReadLine();
+            byte[] buffer = Encoding.UTF8.GetBytes(message);
+            Console.WriteLine($"Server: {message}");
+            stream.Write(buffer, 0, buffer.Length);
+        }
+    }
+
+}
